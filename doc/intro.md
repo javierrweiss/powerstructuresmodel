84ff6736b78c83ba62214a79c219cc8f8471f10a
Javier Blanco <javierweiss@gmail.com>
2022-08-07 21:07:40 -0300

    primer commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to javierweiss/powerstructuresmodel
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/javierweiss/powerstructuresmodel_test.clj
@@ -0,0 +1,7 @@
+(ns javierweiss.powerstructuresmodel-test
+  (:require [clojure.test :refer :all]
+            [javierweiss.powerstructuresmodel :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
