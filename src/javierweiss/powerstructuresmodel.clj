9de7916ff59db1e24bf866df924ca09c4b33e301
Jose Javier Blanco Rivero <javierweiss@gmail.com>
2022-08-08 02:35:38 +0000

    setup clerk

--- src/javierweiss/powerstructuresmodel.clj
+++ src/javierweiss/powerstructuresmodel.clj
@@ -1,5 +1,9 @@
 (ns javierweiss.powerstructuresmodel
+  (:require [nextjournal.clerk :as clerk])
   (:gen-class))
+
+(clerk/serve!{:browse? true})
+(clerk/show! "src/javierweiss/powerstructuresmodel.clj")
 
 (defn greet
   "Callable entry point to the application."
